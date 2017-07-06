db939c7c8dda0d5627fc632ccdfcb2452d163f3a
znergy <ryanjonesirl@gmail.com>
2017-07-05 22:55:48 -0700

    add minor change

--- demo_create_db_mongo.js
+++ demo_create_db_mongo.js
@@ -3,6 +3,6 @@ var url = "mongodb://localhost:27017/mydb";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
+  console.log("Database created");
   db.close();
 });
